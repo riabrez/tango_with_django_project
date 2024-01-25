3e5c8709a16b126e41bbdc5d88d576be93dd6674
Maria Brzezinska <2775727b@student.gla.ac.uk>
2024-01-25 12:23:56 +0000

    Chapter 4 - Templates and Media Files Complete

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key 'boldmessage' matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return rendered response to send to client.
+    # We make use of shortcut function to make our lives easier.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
